9b5d953795d420eb4e96e5b85a9a766a9fb85efa
Katry0_0 <dryzhakova.kt@gmail.com>
2024-11-14 17:45:54 +0100

    fixed endpoint teacher/room/all

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -15,10 +15,6 @@ namespace WebAppsMoodle.Controllers
     public class MainController : ControllerBase
     { 
 
-
-
-
-
         private readonly ILogger<MainController> _logger;
         private readonly DataContext _context;
 
@@ -96,7 +92,32 @@ namespace WebAppsMoodle.Controllers
             if (string.IsNullOrEmpty(teacherId)) return BadRequest("Teacher ID is missing.");
 
             var existingRoom = await _context.Rooms.SingleOrDefaultAsync(r => r.RoomNumber == model.RoomNumber);
-            //if (existingRoom == null) { return BadRequest("Room does not exist."); } проверка сушествует ли уже в такой день и время занятие если нет создаем 
+
+            //if (existingRoom == null) { return BadRequest("Room does not exist."); }
+
+           // o.OneTimeClassEndTime >= model.OneTimeClassStartTime у времени разніе модели
+         /*   // Проверяем, что комната не занята в указанное время
+            bool isRoomOccupied = _context.Classes
+            .Include(c => c.OneTimeClassDates)
+            .Include(c => c.RecurringClassDates)
+            .Where(c => c.RoomId == existingRoom.RoomId) // Фильтруем по номеру комнаты
+            .Any(c =>
+                // Проверка для одноразовых занятий
+                c.OneTimeClassDates.Any(o =>
+                    o.OneTimeClassFullDate.Value.Date == model.OneTimeClassFullDate.Value.Date &&
+                    o.OneTimeClassStartTime <= model.OneTimeClassEndTime &&
+                    o.OneTimeClassEndTime >= model.OneTimeClassStartTime
+                ) ||
+
+                // Проверка для повторяющихся занятий
+                c.RecurringClassDates.Any(r =>
+                    r.RecurrenceDay == model.RecurrenceDay &&
+                    r.RecurrenceStartTime <= model.RecurrenceEndTime &&
+                    r.RecurrenceEndTime >= model.RecurrenceStartTime &&
+                    (r.IsEveryWeek ||
+                     (r.IsEven == model.IsEven)) // Учет четности недели
+                )
+            );*/
 
             Room newRoom;
             Classes newClass;
@@ -105,11 +126,9 @@ namespace WebAppsMoodle.Controllers
             // Если комната не существует, создаем новую
             if (existingRoom == null)
             {
-                newRoom = new Room
-                {  
-                    RoomNumber = model.RoomNumber
-                };
-
+                //only digits
+                if (!int.TryParse(model.RoomNumber, out _))throw new ArgumentException("Room number must contain only digits.");
+                newRoom = new Room {   RoomNumber = model.RoomNumber };
                 await _context.Rooms.AddAsync(newRoom);
             }
             else
@@ -254,67 +273,123 @@ namespace WebAppsMoodle.Controllers
             return Ok(result);*/
         }
         // Endpoint to get all classes for a specific teacher
+        //[HttpGet("teacher/room/all")]
+        /*   public async Task<IActionResult> GetClassesForTeacher()
+           {
+               // Получаем TeacherId из сессии
+               var teacherId = HttpContext.Session.GetString("TeacherId");
+
+               if (string.IsNullOrEmpty(teacherId)) return BadRequest("Teacher ID is missing.");
+
+               var classes = await _context.Classes
+                 .Include(c => c.Room) // Подключаем информацию о комнате
+                 .Include(c => c.ClassesDescription) // Подключаем информацию о описании занятия
+                 .Where(c => c.TeacherId == teacherId)
+                 .Select(c => new
+                 {
+
+                     RoomNumber = c.Room.RoomNumber,
+                     TeacherName = c.Teacher.Username,
+                     ClassTitle = c.ClassesDescription.Title,
+                     IsCanceled = c.IsCanceled
+                 })
+                 .ToListAsync();
+
+               if (classes.Count == 0) return NotFound("No classes found for this teacher.");*/
+
+        //!!!!!!!!!!!!!!! сделать вывод названия, описания и кабинет, IsCanceled or not
+        // Получаем все занятия для данного преподавателя
+
+        /*        var classes = await _context.Classes
+                         .Include(c => c.Room)               // Подключаем информацию о комнате
+                         .Include(c => c.Teacher)            // Подключаем информацию о преподавателе
+                         .Include(c => c.ClassesDescriptions) // Подключаем информацию о занятии
+                         .Where(c => c.TeacherId == teacherId)
+                         .Select(c => new
+                         {
+                             RoomNumber = c.Room.RoomNumber,
+                             TeacherName = c.Teacher.Title,
+                             ClassTitle = c.ClassesDescriptions.Select(d => d.Title).FirstOrDefault(),
+                             IsCanceled = c.IsCanceled
+                         })
+                         .ToListAsync();
+
+                var classes = await _context.Classes
+                    .Include(c => c.Room) // Подключаем информацию о комнате
+                    .Include(c => c.ClassesDescription) // Подключаем информацию о описании занятия
+                    .Where(c => c.TeacherId == teacherId)
+                    .ToListAsync();
+
+
+
+                              var recurringClasses = await _context.RecurringClasses
+                                    .Where(rc => classes.Select(c => c.ClassesId).Contains(rc.ClassesId))
+                                    .ToListAsync();
+
+                                var oneTimeClasses = await _context.OneTimeClasses
+                                    .Where(oc => classes.Select(c => c.ClassesId).Contains(oc.ClassesId))
+                                    .ToListAsync();*/
+
+        /*return Ok(new { Classes = classes, RecurringClasses = recurringClasses, OneTimeClasses = oneTimeClasses});
+    } */
         [HttpGet("teacher/room/all")]
-        public async Task<IActionResult> GetClassesForTeacher()
+        public async Task<IActionResult> GetClassesForTeacher(string teacherId)
         {
             // Получаем TeacherId из сессии
-            var teacherId = HttpContext.Session.GetString("TeacherId");
-
+            //var teacherId = HttpContext.Session.GetString("TeacherId");
+            //"teacherId": " 643b2240-51b9-466c-8752-676e563441b5 "
             if (string.IsNullOrEmpty(teacherId)) return BadRequest("Teacher ID is missing.");
 
+         
+            var checkTeacherId = await _context.Teachers
+                .AsNoTracking()
+                .SingleOrDefaultAsync(r => r.TeacherId == teacherId);
+
+            if (checkTeacherId == null)  return NotFound("Teacher not found.");
+
             var classes = await _context.Classes
-              .Include(c => c.Room) // Подключаем информацию о комнате
-              .Include(c => c.ClassesDescription) // Подключаем информацию о описании занятия*/
+              .Include(c => c.Room) 
+              .Include(c => c.ClassesDescription) 
+              .Include(c => c.OneTimeClassDates)
+              .Include(c => c.RecurringClassDates)
               .Where(c => c.TeacherId == teacherId)
-              .Select(c => new
-              {
-                  
-                  RoomNumber = c.Room.RoomNumber,
-                  TeacherName = c.Teacher.Username,
-                  ClassTitle = c.ClassesDescription.Title,
-                  IsCanceled = c.IsCanceled
-              })
               .ToListAsync();
 
             if (classes.Count == 0) return NotFound("No classes found for this teacher.");
 
-            //!!!!!!!!!!!!!!! сделать вывод названия, описания и кабинет, IsCanceled or not
-            // Получаем все занятия для данного преподавателя
-
-            /* var classes = await _context.Classes
-                 *//*.Include(c => c.Room)               // Подключаем информацию о комнате
-                 .Include(c => c.Teacher)            // Подключаем информацию о преподавателе
-                 .Include(c => c.ClassesDescriptions) // Подключаем информацию о занятии*//*
-                 .Where(c => c.TeacherId == teacherId)
-                 .Select(c => new
-                 {
-                    *//* RoomNumber = c.Room.RoomNumber,
-                     TeacherName = c.Teacher.Title,
-                     ClassTitle = c.ClassesDescriptions.Select(d => d.Title).FirstOrDefault(),*//*
-                     IsCanceled = c.IsCanceled
-                 })
-                 .ToListAsync();*/
-
-            /*  var classes = await _context.Classes
-                  *//*.Include(c => c.Room) // Подключаем информацию о комнате
-                  .Include(c => c.ClassesDescription) // Подключаем информацию о описании занятия*//*
-                  .Where(c => c.TeacherId == teacherId)
-                  .ToListAsync();*/
+            var result = classes.Select(c => new
+            {
+                ClassTitle = c.ClassesDescription.Title,
+                ClassId = c.ClassesId,
+                RoomNumber = c.Room.RoomNumber,
+                RoomId = c.Room.RoomId,
+                TeacherName = checkTeacherId.Username,
+                TeacherId = checkTeacherId.TeacherId,
+                IsCanceled = c.IsCanceled,
 
 
+              RecurringClasses = c.RecurringClassDates.Select(r => new
+                {
+                    RecurrenceDay = r.RecurrenceDay,
+                    RecurrenceStartTime = r.RecurrenceStartTime,
+                    RecurrenceEndTime = r.RecurrenceEndTime,
+                    IsEven = r.IsEven,
+                    IsEveryWeek = r.IsEveryWeek
+                }).ToList(),
 
-            /*            var recurringClasses = await _context.RecurringClasses
-                            .Where(rc => classes.Select(c => c.ClassesId).Contains(rc.ClassesId))
-                            .ToListAsync();
+                 OneTimeClasses = c.OneTimeClassDates.Select(o => new
+                {
+                    OneTimeClassFullDate = o.OneTimeClassFullDate,
+                    OneTimeClassStartTime = o.OneTimeClassStartTime,
+                    OneTimeClassEndTime = o.OneTimeClassEndTime,
 
-                        var oneTimeClasses = await _context.OneTimeClasses
-                            .Where(oc => classes.Select(c => c.ClassesId).Contains(oc.ClassesId))
-                            .ToListAsync();*/
+                }).ToList(),
+            }).ToList();
 
-            return Ok(new { Classes = classes/*, RecurringClasses = recurringClasses, OneTimeClasses = oneTimeClasses */});
+            return Ok(result);
         }
-       
-        [HttpGet("room/all")]
+
+            [HttpGet("room/all")]
         public async Task<IActionResult> GetAllRoomsData()
         {
             // Получаем все кабинеты с полями RoomId и RoomNumber
@@ -370,6 +445,64 @@ namespace WebAppsMoodle.Controllers
 
         }
 
+        /*  // Endpoint to get all classes for a specific room
+          [HttpGet("classes/room/id")]
+          public async Task<IActionResult> GetClassesForRoom(string roomNumber)
+          {
+              var roomId = await GetRoomIdByRoomNumberAsync(roomNumber);
+              // Сначала проверяем, существует ли комната с данным номером
+              var room = await _context.Rooms
+                  .AsNoTracking()  // Убираем отслеживание для оптимизации запроса
+                  .SingleOrDefaultAsync(r => r.RoomNumber == roomNumber);
+              Console.WriteLine(room);
+
+              // Если комната не найдена, возвращаем сообщение об ошибке
+              if (room == null)
+              {
+                  return BadRequest("Room does not exist.");
+              }
+
+
+              // Получаем все занятия, привязанные к RoomId этой комнаты
+              var classes = await _context.Classes
+                  .Where(c => c.RoomId == roomId) // Используем RoomId, найденный в предыдущем запросе
+                   .Include(c => c.ClassesDescription)  // Присоединяем информацию о описании занятия, если требуется
+                   .Select(c => new
+                   {
+                       //ClassTitle = c.ClassesDescriptions.Select(d => d.Title).FirstOrDefault()
+                       RoomNumber = c.Room.RoomNumber,
+                       TeacherName = c.Teacher.Username,
+                       ClassTitle = c.ClassesDescription.Title,
+                       RoomDescription = c.ClassesDescription.Description  
+                   })
+                  .ToListAsync();
+
+              // Проверяем, есть ли занятия для данной комнаты
+              if (classes.Count == 0)
+              {
+                  return NotFound("No classes found for this room.");
+              }
+
+              // Возвращаем список занятий
+              return Ok(classes);
+
+              //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! нужно выводить по айди а не по комнате сделать проверку и вывод
+              var classes = await _context.Classes
+                 .Where(c => c.RoomId == room.RoomId)
+                 //.Include(c => c.ClassesDescription) // Подключаем описание занятия
+                 //.Include(c => c.Teacher) // Подключаем информацию о преподавателе, если нужно
+                 .ToListAsync();
+
+
+
+              if (classes.Count == 0)
+              {
+                  return NotFound("No classes found for this room.");
+              }
+
+              return Ok(classes);
+          }*/
+
         // Endpoint to get all classes for a specific room
         [HttpGet("classes/room/id")]
         public async Task<IActionResult> GetClassesForRoom(string roomNumber)
@@ -398,7 +531,7 @@ namespace WebAppsMoodle.Controllers
                      RoomNumber = c.Room.RoomNumber,
                      TeacherName = c.Teacher.Username,
                      ClassTitle = c.ClassesDescription.Title,
-                     RoomDescription = c.ClassesDescription.Description  
+                     RoomDescription = c.ClassesDescription.Description
                  })
                 .ToListAsync();
 
@@ -412,20 +545,20 @@ namespace WebAppsMoodle.Controllers
             return Ok(classes);
 
             //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! нужно выводить по айди а не по комнате сделать проверку и вывод
-           /* var classes = await _context.Classes
-               .Where(c => c.RoomId == room.RoomId)
-               //.Include(c => c.ClassesDescription) // Подключаем описание занятия
-               //.Include(c => c.Teacher) // Подключаем информацию о преподавателе, если нужно
-               .ToListAsync();
-     
-    
+            /* var classes = await _context.Classes
+                .Where(c => c.RoomId == room.RoomId)
+                //.Include(c => c.ClassesDescription) // Подключаем описание занятия
+                //.Include(c => c.Teacher) // Подключаем информацию о преподавателе, если нужно
+                .ToListAsync();
 
-            if (classes.Count == 0)
-            {
-                return NotFound("No classes found for this room.");
-            }
 
-            return Ok(classes);*/
+
+             if (classes.Count == 0)
+             {
+                 return NotFound("No classes found for this room.");
+             }
+
+             return Ok(classes);*/
         }
         private async Task<string> GetRoomIdByRoomNumberAsync(string roomNumber)
         {
@@ -623,6 +756,7 @@ namespace WebAppsMoodle.Controllers
         [HttpGet("classes/date/OneTimeClass")]
         public async Task<IActionResult> GetOneTimeClassByDate(DateTime date)
         {
+
             var classes = await _context.Classes
                 .Include(c => c.ClassesDescription) // Подключаем информацию о описании занятия
                 .Include(c => c.Teacher) // Подключаем информацию о преподавателе
--- Program.cs
+++ Program.cs
@@ -7,6 +7,75 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+    });
+// Add Swagger for API documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Entity Framework Core
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services for session support
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // Allow frontend
+                  .AllowAnyHeader()
+                  .AllowAnyMethod(); // Allow all HTTP methods
+        });
+
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()   // Allow any origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+// Enable session middleware
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("AllowAllOrigins");
+
+app.Run();
+
+/*using Microsoft.EntityFrameworkCore;
+using WebAppsMoodle.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
         // ”казываем, что нужно учитывать пустые значени€ (null) в JSON
         options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
     });
@@ -45,7 +114,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run();*/
 
 
 /*using Microsoft.EntityFrameworkCore;
